7cb7c3ecc6817e44f4dd78c9b0ff2ec9ab36699c
Legenda759 <oripov1994@gmail.com>
2022-03-07 10:02:59 +0500

    Initial Commit

---
+++ ToDoUIkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoUIkit
+//
+//  Created by Legenda_759 on 07/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
